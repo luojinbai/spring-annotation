6718d7444b1f1a2111d63bc48e5446725b3f4163
luojinbai <sunflower122>
2019-01-08 22:28:36 +0800

    解决冲突

--- src/main/java/com/yibai/spring/annotation/bean/Person.java
+++ src/main/java/com/yibai/spring/annotation/bean/Person.java
@@ -3,16 +3,21 @@ package com.yibai.spring.annotation.bean;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Setter
 @Getter
 @ToString
-//@Slf4j
+@Slf4j
 public class Person {
 
 	private String name;
 	private int age;
 	private Address address;
+
+	public Person() {
+		log.debug("构造器");
+	}
 
 	public void init() {
 //		log.debug("初始化");
